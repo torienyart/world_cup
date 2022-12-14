e42f76fda57681477e389743e47b1aea4eb2dd16
Tori Enyart <torienyart@gmail.com>
2022-12-14 12:47:58 -0700

    feat: Add method to initialize WC'

---
+++ lib/world_cup.rb
@@ -0,0 +1,9 @@
+class WorldCup
+    attr_reader :year,
+                :teams
+
+    def initialize(year, teams)
+        @year = year
+        @teams = teams
+    end 
+end
---
+++ spec/world_cup_spec.rb
@@ -0,0 +1,37 @@
+require'./lib/player'
+require'./lib/team'
+require'./lib/world_cup'
+
+describe WorldCup do
+
+
+    let(:france) {Team.new("France")}
+    let(:mbappe) {Player.new({name: "Kylian Mbappe", position: "forward"})}
+    let(:pogba) {Player.new({name: "Paul Pogba", position: "midfielder"})}
+
+    let(:croatia) {Team.new("Croatia")}
+    let(:vida) {Player.new({name: "Domagoj Vida", position: "defender"})}
+    let(:modric) {Player.new({name: "Luka Modric", position: "midfielder"})}
+
+    let(:world_cup) {WorldCup.new(2018, [france, croatia])}
+
+    it 'can initialize' do
+        expect(world_cup).to be_an_instance_of(WorldCup)
+        expect(world_cup.year).to eq(2018)
+        expect(world_cup.teams).to eq([france, croatia])
+    end
+
+    it 'can sort active players by position' do
+        france.add_player(mbappe)
+        france.add_player(pogba)
+        croatia.add_player(modric)
+        croatia.add_player(vida)
+
+        expect(world_cup.active_players_by_position("midfielder")).to match[pogba, modric]
+        
+        croatia.eliminated = true
+
+        expect(world_cup.active_players_by_position("midfielder")).to eq([pogba])
+    end
+
+end
